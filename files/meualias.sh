6ffdd318b33e761910f0fe0d73403f8cc5cffd86
Landir <landir@gmail.com>
2022-07-04 19:42:48 -0300

    mvn coverage alias

--- files/meualias.sh
+++ files/meualias.sh
@@ -2,9 +2,10 @@
 alias config='/usr/bin/git --git-dir=$HOME/.meucfg --work-tree=$HOME'
 alias dockerip='docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" '
 alias pythonserver='python3 -m http.server'
-alias ci="mvn -Dmaven.test.skip=true  -DskipTests -P local clean verify"
+alias ci="mvn -Dmaven.test.skip=true -DskipTests -Plocal clean verify"
 alias sr="mvn spring-boot:run"
 alias dockerrm="docker-compose down --remove-orphans --volumes"
+alias cobertura="mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true && mvn org.jacoco:jacoco-maven-plugin:report test"
 
 # exports
 export GIT_PAGER="cut -c 1-${COLUMNS-80}"
